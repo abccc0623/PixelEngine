13560fc5115394fd340e357723c283ab68a6c32c
KimHyungSun <abccc0623@naver.com>
2026-03-01 22:08:45 +0900

    [형선] Tool 색변경

--- PixelSolution/PixelTool/MainWindow.xaml.cs
+++ PixelSolution/PixelTool/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -17,6 +18,13 @@ namespace PixelTool
         public MainWindow()
         {
             InitializeComponent();
+
+            IntPtr hWnd = new WindowInteropHelper(this).Handle;
+            int darkMode = 1; // 1이면 다크, 0이면 라이트
+            DwmSetWindowAttribute(hWnd, 35, ref darkMode, sizeof(int)); // 35: DWMWA_USE_IMMERSIVE_DARK_MODE
         }
+
+        [DllImport("dwmapi.dll")]
+        private static extern int DwmSetWindowAttribute(IntPtr hwnd, int attr, ref int attrValue, int attrSize);
     }
 }
